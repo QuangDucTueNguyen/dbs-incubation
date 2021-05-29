8b036360e805c63d747ba07e2b14f9d9b3432099
Quang Nguyen <Quang_Nguyen@epam.com>
2021-05-29 22:02:56 +0700

    init authentication service

---
+++ authenticate-service/src/main/java/com/incubator/dbs/authenticateservice/AuthenticateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.incubator.dbs.authenticateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticateServiceApplication.class, args);
+	}
+
+}
